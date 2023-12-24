699bb969a3ff6eb8a4f5c393bdfcc12d6b756f90
S J K <46325950+Suu-J@users.noreply.github.com>
2023-12-24 09:26:43 +0530

    Update Rules.java

--- src/quiz/application/Rules.java
+++ src/quiz/application/Rules.java
@@ -25,14 +25,14 @@ public class Rules extends JFrame implements ActionListener{
         rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
         rules.setText(
             "<html>"+ 
-                "1. You are trained to be a programmer and not a story teller, answer point to point" + "<br><br>" +
-                "2. Do not unnecessarily smile at the person sitting next to you, they may also not know the answer" + "<br><br>" +
-                "3. You may have lot of options in life but here all the questions are compulsory" + "<br><br>" +
-                "4. Crying is allowed but please do so quietly." + "<br><br>" +
-                "5. Only a fool asks and a wise answers (Be wise, not otherwise)" + "<br><br>" +
-                "6. Do not get nervous if your friend is answering more questions, may be he/she is doing Jai Mata Di" + "<br><br>" +
-                "7. Brace yourself, this paper is not for the faint hearted" + "<br><br>" +
-                "8. May you know more than what John Snow knows, Good Luck" + "<br><br>" +
+                "1. Do not cheat!" + "<br><br>" +
+                "2. Do not disturb others!" + "<br><br>" +
+                "3. All the questions are compulsory" + "<br><br>" +
+                "4. There is only one correct option" + "<br><br>" +
+                "5. Time is ticking! Make sure you attempt before the clock runs out" + "<br><br>" +
+                "6. No Negative grading" + "<br><br>" +
+                "7. Focus!" + "<br><br>" +
+                "8. Good Luck" + "<br><br>" +
             "<html>"
         );
         add(rules);
